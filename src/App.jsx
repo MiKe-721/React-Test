aa5d3245f4b93347b5d960d1571fdaeaa7e034aa
Test <mikecryptoacc267@gmail.com>
2023-03-12 14:12:09 +0000

    Lesson48

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  console.log("最初");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setfaceShowFlag] = useState(true);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+
+  const onClickSwitchfaceShowFlag = () => {
+    setfaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num % 3 === 0) {
+      faceShowFlag || setfaceShowFlag(true);
+    } else {
+      faceShowFlag && setfaceShowFlag(false);
+    }
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchfaceShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>😟</p>}
     </>
   );
 };
